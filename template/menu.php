eb51b7923d9667d6e0ecbc15a3ad1f5b064113b5
Albertkeiv <Alvertkeiv@gmail.com>
2021-04-12 13:26:59 +0600

    add nav-menu beta

--- template/menu.php
+++ template/menu.php
@@ -1,15 +1,15 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <ul class="nav me-auto mb-2 mb-lg-0 nav-pills">
             <li class="nav-item">
-                <a class="nav-link active shadow-sm bg-body rounded" aria-current="page" href="/team.php">Команды</a>
+                <a class="nav-link <?php if ($_SERVER['SCRIPT_NAME']=="/team.php") echo "active"; ?>" aria-current="page" href="/team.php">Команды</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link active shadow-sm bg-body rounded" aria-current="page" href="php-scripts/temp.php">Second</a>
+                <a class="nav-link <?php if ($_SERVER['SCRIPT_NAME']=="/php-scripts/temp.php") echo "active"; ?>" aria-current="page" href="/php-scripts/temp.php">Second</a>
             </li>
         </ul>
         <span class="navbar-text">Привет, <?php echo $_COOKIE['user']; ?>! </span>
         <span class="border-end border-5"></span>
-        <a class="nav-link active shadow-sm bg-body rounded" href="../php-scripts/logout.php">Выход</a>
+        <a class="nav-link" href="../php-scripts/logout.php">Выход</a>
     </div>
 </nav>
