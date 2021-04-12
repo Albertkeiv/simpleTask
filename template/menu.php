77c8bdc06142847e6c4be90e40ab385703850f1f
Albertkeiv <Alvertkeiv@gmail.com>
2021-04-12 10:35:42 +0600

    Update menu.php

--- template/menu.php
+++ template/menu.php
@@ -2,12 +2,14 @@
     <div class="container-fluid">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/main.php">Home</a>
+                <a class="nav-link active shadow-sm bg-body rounded" aria-current="page" href="/team.php">Команды</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="php-scripts/temp.php">Second</a>
+                <a class="nav-link active shadow-sm bg-body rounded" aria-current="page" href="php-scripts/temp.php">Second</a>
             </li>
         </ul>
-        <span class="navbar-text"> <?php echo $_COOKIE['user']; ?> </span>
+        <span class="navbar-text">Привет, <?php echo $_COOKIE['user']; ?>! </span>
+        <span class="border-end border-5"></span>
+        <a class="nav-link active shadow-sm bg-body rounded" href="../php-scripts/logout.php">Выход</a>
     </div>
 </nav>
